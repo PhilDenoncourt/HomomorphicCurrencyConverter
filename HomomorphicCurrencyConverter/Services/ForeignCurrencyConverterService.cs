b45f0e022dcb1865dbd872caa7730563d9e08603
Phil Denoncourt <phil@denoncourtassociates.com>
2022-11-12 06:21:00 -0500

    Initail work

---
+++ HomomorphicCurrencyConverter/Services/ForeignCurrencyConverterService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomomorphicCurrencyConverter.Protos;
+using Microsoft.Research.SEAL;
+using Grpc.Core;
+
+namespace HomomorphicCurrencyConverter.Services
+{
+    public class ForeignCurrencyConverterService: ForeignCurrencyConvertorService.ForeignCurrencyConvertorServiceBase
+    {
+        public override Task<EncryptionParametersResponse> GetEncryptionParameters(EncryptionParametersRequest req, ServerCallContext context)
+        {
+            var response = new EncryptionParametersResponse()
+            {
+                ParmStream = Google.Protobuf.ByteString.CopyFrom(GetParamsStream())
+            };
+
+            return Task.FromResult(response);
+        }
+
+        private byte[] GetParamsStream()
+        {
+            using MemoryStream parmsStream = new MemoryStream();
+
+            ulong polyModulusDegree = 8192;
+            using EncryptionParameters parms = new EncryptionParameters(SchemeType.CKKS);
+            parms.PolyModulusDegree = polyModulusDegree;
+            parms.CoeffModulus = CoeffModulus.Create(
+                polyModulusDegree, new int[] { 50, 20, 50 });
+
+            var sz = parms.Save(parmsStream);
+            parmsStream.Seek(0, SeekOrigin.Begin);
+
+            return parmsStream.ToArray();
+        }
+
+        public override Task<ConvertResponse> ConvertUsdToCad(ConvertRequest request, ServerCallContext context)
+        {
+            //Load the parameters, and setup the streams
+            using EncryptionParameters parms = new EncryptionParameters();
+            using var parmsStream = new MemoryStream(GetParamsStream());
+            using var dataStream = new MemoryStream(request.DataStream.ToByteArray());
+
+            parms.Load(parmsStream);
+
+            
+            //Setup the environment
+            using SEALContext sealContext = new SEALContext(parms);
+            using Evaluator evaluator = new Evaluator(sealContext);
+
+            //Load the keys, and encrypted data
+            using RelinKeys rlk = new RelinKeys();
+            using Ciphertext encryptedInput = new Ciphertext();
+
+            rlk.Load(sealContext, dataStream);
+            encryptedInput.Load(sealContext, dataStream);
+
+            
+            //Set up the encoder
+            using var encoder = new CKKSEncoder(sealContext);
+
+            
+            //Setup the conversion rate so that it can be used for homomorphic operations
+            using var conversionRate = new Plaintext();
+            double usdToCad = 1.33;
+            double scale = Math.Pow(2.0, 20);
+            encoder.Encode(usdToCad, scale, conversionRate);
+
+            
+            //Do the business logic
+            using var encryptedResult = new Ciphertext();
+            evaluator.MultiplyPlain(encryptedInput, conversionRate, encryptedResult);
+
+            
+            //Relinearize and rescale to save space
+            evaluator.RelinearizeInplace(encryptedResult, rlk);
+            evaluator.RescaleToNextInplace(encryptedResult);
+
+            
+            //Save the encrypted results to the stream
+            using var resultStream = new MemoryStream();
+
+            _ = encryptedResult.Save(resultStream);
+
+            
+            //Send it back to the client.
+            return Task.FromResult(new ConvertResponse()
+            {
+                DataStream = Google.Protobuf.ByteString.CopyFrom(resultStream.ToArray())
+            });
+        }
+
+    }
+}
---
+++ HomomorphicCurrencyConverterClient/Program.cs
@@ -0,0 +1,100 @@
+﻿using Grpc.Net.Client;
+using HomomorphicCurrencyConverter.Protos;
+using Microsoft.Research.SEAL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HomomorphicCurrencyConverterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Get the secret amount
+            Console.WriteLine("Enter the amount of USD to securely convert to CAD");
+            var input = Console.ReadLine();
+
+            double usdAmount = Convert.ToDouble(input);
+
+            
+            //Get encryption params from the server so we're all speaking correctly to each other
+            var encryptionParms = await GetEncryptionParameters();
+            using var encryptionParamsStream = new MemoryStream(encryptionParms);
+            
+            using EncryptionParameters parms = new EncryptionParameters();
+            parms.Load(encryptionParamsStream);
+
+
+            //Setup my secret and public keys using helper methods.  Also create and store the relinearization keys
+            using SEALContext context = new SEALContext(parms);
+
+            using KeyGenerator keygen = new KeyGenerator(context);
+            using SecretKey sk = keygen.SecretKey;
+            keygen.CreatePublicKey(out PublicKey pk);
+
+            using Serializable<RelinKeys> rlk = keygen.CreateRelinKeys();
+            using var dataStream = new MemoryStream();
+
+            _ = rlk.Save(dataStream);
+
+            
+            //Create the encoder and store the soon to be secured input
+            CKKSEncoder encoder = new CKKSEncoder(context);
+            using Plaintext plainInput = new Plaintext();
+            double scale = Math.Pow(2.0, 20);
+            encoder.Encode(usdAmount, scale, plainInput);
+
+            
+            //Encrypt the plaintext
+            using Encryptor encryptor = new Encryptor(context, pk);
+            _ = encryptor.Encrypt(plainInput).Save(dataStream);
+
+            
+            //Call the service
+            dataStream.Seek(0, SeekOrigin.Begin);
+            var cadEncryptedResult = await CallService(dataStream.ToArray());
+            using var cadEncryptedStream = new MemoryStream(cadEncryptedResult);
+
+
+            //Decrypt the results
+            using Decryptor decryptor = new Decryptor(context, sk);
+            
+            using Ciphertext encryptedResult = new Ciphertext();
+            encryptedResult.Load(context, cadEncryptedStream);
+
+            using Plaintext plainResult = new Plaintext();
+            decryptor.Decrypt(encryptedResult, plainResult);
+
+            var result = new List<double>();
+            encoder.Decode(plainResult, result);
+
+
+            //Display results
+            var cad = result[0].ToString("C");
+            Console.WriteLine($"${input} USD = {cad} CAD");
+            Console.ReadKey();
+        }
+
+        private async static Task<byte[]> GetEncryptionParameters()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ForeignCurrencyConvertorService.ForeignCurrencyConvertorServiceClient(channel);
+            var req = new EncryptionParametersRequest();
+            var clientResult = await client.GetEncryptionParametersAsync(req);
+
+            return clientResult.ParmStream.ToByteArray();
+        }
+
+        private async static Task<byte[]> CallService(byte[] dataStream)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ForeignCurrencyConvertorService.ForeignCurrencyConvertorServiceClient(channel);
+            var req = new ConvertRequest();
+            req.DataStream = Google.Protobuf.ByteString.CopyFrom(dataStream);
+            var response = await client.ConvertUsdToCadAsync(req);
+            return response.DataStream.ToByteArray();
+        }
+    }
+}
